c0b7bf97b3aba4b4c02741601a84658a9a45f3ec
par55 <par555@mail.ru>
2022-11-11 20:39:27 +0100

    homework Asterics Obelics Potion

---
+++ src/astericsObelics.java
@@ -0,0 +1,9 @@
+public class astericsObelics {
+    public static void main(String[] args) {
+        int goodPotion = 5;
+        int astericsPotion = 3;
+        float obelicsPotion = astericsPotion / 2;
+        boolean goodJob = ((astericsPotion + obelicsPotion) >= goodPotion);
+        System.out.println(goodJob);
+    }
+}
